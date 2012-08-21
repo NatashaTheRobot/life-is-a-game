d0a827d7559e0f31ae00586edd5bd38665a6489f
NatashaMurashev <nmurashev@gmail.com>
2012-08-21 14:12:44 -0700

    hardcoding row check

--- game.js
+++ game.js
@@ -1,71 +1,75 @@
 $(function(){
-	var COMPUTER = 0;
-	var OPPONENT = 1;
+	var COMPUTER = 0; //computer is Os
+	var OPPONENT = 1; //opponent is Xs
 	
-	var board = {}
+	var board = {};
+	var row1, row2, row3, column1, column2, column3, diagonal1, diagonal2;
 	
 	function numFromWord(word) {
 		var regex = new RegExp(/\d/)
 		return parseInt(regex.exec(word))
 	}
 	
-	
-	function findWinningCells(x, y) {
-		var winner = {}
-		var x1, x2, y1, y2;
-		
-		//winner row cell x's
-		if(x === 0 ) {
-			x1 = 1;
-			x2 = 2;
-		} else if(x === 1) {
-			x1 = 0;
-			x2 = 2;
-		} else {
-			x1 = 0;
-			x2 = 1;
-		};
-		
-		//winner column cell y's
-		if(y === 0) {
-			y1 = 1;
-			y2 = 2;
-		} else if(y === 1) {
-			y1 = 0;
-			y2 = 2;
-		} else {
-			y1 = 0;
-			y2 = 1;
-		};
-		
-		winner.row = {cell1: {x: x1, y: y}, cell2: {x: x2, y: y}};
-		winner.column = {cell1: {x: x, y: y1}, cell2: {x: x, y: y2}};
-		
-		//add diagonal winners for relavent cells
-		var centerCell = {x: 1, y: 1}
-		if(x === 1 && y === 1) {
-			winner.diagonal1 = {cell1: {x: 0, y: 2}, cell2: {x: 2, y: 0}};
-			winner.diagonal2 = {cell1: {x: 2, y: 2}, cell2: {x: 0, y: 0}}; 
-		}
-		
-	}
-	
+	//adds a cell object to the board
 	function addCellToBoard(cell) {
 		var cellName = cell.attr('id');
 		var id = numFromWord(cellName);
 		var coordinates = cell.attr('coordinates').split('-');
 		var x = numFromWord(coordinates[0]);
 		var y = numFromWord(coordinates[1]);
-		var winningCells = findWinningCells(x, y)
 		board.cellName = {id: id, x: x, y: y, content: OPPONENT};
 	}
+	
+	//check rows for winner
+	function winnerRow(){
+		if(row1 === undefined) {
+			if('cell1' in board && 'cell2' in board && 'cell3' in board){
+				 if(board.cell1.content === board.cell2.content === board.cell3.content){
+					alert('somebody won!!!!')
+				} else {
+					row1 = false
+				}
+			}
+		}
+		if(row2 === undefined) {
+			if('cell4' in board && 'cell5' in board && 'cell6' in board){
+				 if(board.cell4.content === board.cell5.content === board.cell6.content){
+					alert('somebody won!!!!')
+				} else {
+					row2 = false
+				}
+			}
+		}
+		if(row3 === undefined) {
+			if('cell7' in board && 'cell8' in board && 'cell9' in board){
+				 if(board.cell7.content === board.cell8.content === board.cell9.content){
+					alert('somebody won!!!!')
+				} else {
+					row3 = false
+				}
+			}
+		}
+	}
+	
+	function checkForWinner(){
+		checkRows();
+		//if there is no winner returns false
+		//if there is a winner returns true
+	}
+	
 	//plays the next move
 	function play(cell) {
+		$(cell).html("X");
 		$(cell).off('click');
 		addCellToBoard(cell);
+		if(!checkForWinner()) {
+			makeMove();
+		} else {
+			//add jquery to add text for winner
+		}
 	}
 
-	//add event handlers for each cell
+	//add click event handler for each cell
 	$('.span1').click(function(){
 		play($(this));
 	});
