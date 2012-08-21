6c5bb231878fd8547aa2f899b2f0656ab6a58073
NatashaMurashev <nmurashev@gmail.com>
2012-08-21 15:16:13 -0700

    added row, column, and diagonal objects

--- game.js
+++ game.js
@@ -1,58 +1,53 @@
 $(function(){
-	var COMPUTER = 0; //computer is Os
-	var OPPONENT = 1; //opponent is Xs
+	var COMPUTER = 'o'; //computer is Os
+	var OPPONENT = 'x'; //opponent is Xs
 	
-	var board = {};
-	var row1, row2, row3, column1, column2, column3, diagonal1, diagonal2;
+	var cells = {}; // all cells
+	var rows = {row0: {}, row1: {}, row2: {}}; // cells in rows
+	var columns = {column0: {}, column1: {}, column2: {}}; //cells in columns
+	var diagonals = {diagonal1: {}, diagonal2: {}}; //cells in diagonals
 	
 	function numFromWord(word) {
 		var regex = new RegExp(/\d/)
 		return parseInt(regex.exec(word))
 	}
 	
-	//adds a cell object to the board
-	function addCellToBoard(cell) {
+	//checks if the cell is in the diagonals
+	function diagonalPossible(x, y) {
+		if((x === 0 && y == 1) || (x === 1 && y === 2) || (x === 1 && y === 0) || (x === 2 && y === 1)){
+			return false;
+		} else {
+			return true;
+		}
+	}
+	
+	//adds a cell object to the boards
+	function addCellToBoards(cell) {
 		var cellName = cell.attr('id');
 		var id = numFromWord(cellName);
 		var coordinates = cell.attr('coordinates').split('-');
 		var x = numFromWord(coordinates[0]);
 		var y = numFromWord(coordinates[1]);
-		board.cellName = {id: id, x: x, y: y, content: OPPONENT};
-	}
-	
-	//check rows for winner
-	function winnerRow(){
-		if(row1 === undefined) {
-			if('cell1' in board && 'cell2' in board && 'cell3' in board){
-				 if(board.cell1.content === board.cell2.content === board.cell3.content){
-					alert('somebody won!!!!')
-				} else {
-					row1 = false
-				}
-			}
-		}
-		if(row2 === undefined) {
-			if('cell4' in board && 'cell5' in board && 'cell6' in board){
-				 if(board.cell4.content === board.cell5.content === board.cell6.content){
-					alert('somebody won!!!!')
-				} else {
-					row2 = false
-				}
-			}
-		}
-		if(row3 === undefined) {
-			if('cell7' in board && 'cell8' in board && 'cell9' in board){
-				 if(board.cell7.content === board.cell8.content === board.cell9.content){
-					alert('somebody won!!!!')
-				} else {
-					row3 = false
-				}
+		cells.cellName = {id: id, x: x, y: y, content: OPPONENT};
+		var rowName = 'row' + y;
+		rows[rowName][cellName] = cells.cellName;
+		var columnName = 'column' + x;
+		columns[columnName][cellName] = cells.cellName;
+		if(diagonalPossible(x, y)) {
+			if(x == 1 && y == 1){
+				diagonals['diagonal1'][cellName] = cells.cellName;
+				diagonals['diagonal2'][cellName] = cells.cellName;
+			} else if ((x === 0 && y === 2) || (x === 2 && y === 0)) {
+				diagonals['diagonal1'][cellName] = cells.cellName;
+			} else {
+				diagonals['diagonal2'][cellName] = cells.cellName;
 			}
 		}
 	}
+
 	
 	function checkForWinner(){
-		checkRows();
+		//checkRows();
 		//if there is no winner returns false
 		//if there is a winner returns true
 	}
@@ -61,9 +56,9 @@ $(function(){
 	function play(cell) {
 		$(cell).html("X");
 		$(cell).off('click');
-		addCellToBoard(cell);
+		addCellToBoards(cell);
 		if(!checkForWinner()) {
-			makeMove();
+			//makeMove();
 		} else {
 			//add jquery to add text for winner
 		}
