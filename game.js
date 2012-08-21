6f81b348894eb9030cce0f0f3297f04df6f2f1bd
NatashaMurashev <nmurashev@gmail.com>
2012-08-21 10:06:44 -0700

    refactoring javascript

--- game.js
+++ game.js
@@ -1,4 +1,6 @@
 $(function(){
+	var COMPUTER = 0;
+	var OPPONENT = 1;
 	
 	var plays = new Array(9)
 	
@@ -8,49 +10,29 @@ $(function(){
 		return regex.exec(indexString)[0]
 	}
 	
+	function checkForWin() {
+		
+	}
+	
 	function counterMove() {
 		//checks for 2 Self moves in a row for a winning move
+		checkForWin();
 		//checks for 2 Opponent moves in a row for a blocking move
 		//checks for a path of most wins and adds self move
 	}
 	
 	//plays the next move
-	function play(cellid) {
-		$(cellid).off('click');
-		var cellIndex = findIndex($(cellid).attr('name'));
+	function play(cell) {
+		$(cell).off('click');
+		var cellIndex = findIndex($(cell).attr('name'));
 		plays[cellIndex] = 'opponent';
 		console.log(plays)
 		counterMove();
 	}
 
 	//add event handlers for each cell
-	$('#cell02').on('click', function(){
-		play('#cell02')
+	$('.span1').click(function(){
+		alert($(this).attr('id'));
+		play($(this));
 	});
-	$('#cell12').on('click', function(){
-		play('#cell12')
-	});
-	$('#cell22').on('click', function(){
-		play('#cell22')
-	});
-	$('#cell01').on('click', function(){
-		play('#cell01')
-	});
-	$('#cell11').on('click', function(){
-		play('#cell11')
-	});
-	$('#cell21').on('click', function(){
-		play('#cell21')
-	});
-	$('#cell00').on('click', function(){
-		play('#cell00');
-	});
-	$('#cell10').on('click', function(){
-		play('#cell10');
-	});
-	$('#cell20').on('click', function(){
-		play('#cell20');
-	});
-	
-
 });	
