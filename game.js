8ae4ba5aeac9527863f77765ef2f94cad51a850b
NatashaMurashev <nmurashev@gmail.com>
2012-08-21 19:31:21 -0700

    added column block move

--- game.js
+++ game.js
@@ -28,7 +28,6 @@ $(function(){
 		var cellObj = {x: x, y: y, content: OPPONENT};
 		var rowName = 'row' + y;
 		rows[rowName][cell + x] = cellObj;
-		console.log(rows[rowName][cell + x])
 		var columnName = 'column' + x;
 		columns[columnName][cell + y] = cellObj;
 		if(diagonalPossible(x, y)) {
@@ -57,7 +56,7 @@ $(function(){
 		for(cell in elements[element]) {
 			contents.push(elements[element][cell].content);
 		}
-		return contents[0] === contents[1] && contents[1] === contents[2];
+		return (contents[0] === contents[1]) && (contents[1] === contents[2]);
 	}
 	
 	//checks rows, columns, or diagonals for 3 in a row wins
@@ -105,16 +104,32 @@ $(function(){
 		return false;
 	}
 	
+	function addCellToBoardsFromCoordinates(x,y) {
+		var cellObj = {x: x, y: y, content: COMPUTER};
+		var rowName = 'row' + y;
+		rows[rowName]['cell' + y] = cellObj;
+		var columnName = 'column' + x;
+		columns[columnName]['cell' + x] = cellObj;
+		if(diagonalPossible(x, y)) {
+			if(x == 1 && y == 1){
+				diagonals['diagonal1']['cell' + x + y] = cellObj;
+				diagonals['diagonal2']['cell' + y + y] = cellObj;
+			} else if ((x === 0 && y === 2) || (x === 2 && y === 0)) {
+				diagonals['diagonal1']['cell' + x + y] = cellObj;
+			} else {
+				diagonals['diagonal2']['cell' + x + y] = cellObj;
+			}
+		}
+	}
+	
 	//adding the block move to the board
 	function makeBlockMove(blockSection) {
 		var cells = []
 		for(var cell in blockSection) {
 			cells.push(blockSection[cell])
 		}
-		console.log(cells)
 		//same column
 		if(cells[0].x === cells[1].x){
-			coordinates='x1-y2'
 			var y;
 			var sumYs = cells[0].y + cells[1].y
 			if(sumYs === 3) {
@@ -125,9 +140,25 @@ $(function(){
 				y = 2;
 			}
 			var coordinates = '#x' + cells[0].x + '-y' + y;
-			$(coordinates).html('o')
+			$(coordinates).html('o');
+			$(coordinates).off('click');
+			addCellToBoardsFromCoordinates(cells[0].x,y);
+			
 		} else if(cells[0].y === cells[1].y) { //same row
-			alert('same row')
+			var x;
+			var sumXs = cells[0].x + cells[1].x
+			if(sumXs === 3) {
+				x = 0;
+			} else if(sumXs === 2) {
+				x = 1;
+			} else {
+				x = 2;
+			}
+			var coordinates = '#x' + x + '-y' + cells[0].y;
+			$(coordinates).html('o');
+			$(coordinates).off('click');
+			addCellToBoardsFromCoordinates(x,cells[0].y);
+			
 		} else { //same diagonal
 			alert('same diagonal')
 		}
